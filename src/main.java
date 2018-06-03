e91878d9d9b1ad0f5374bf9e747df4d7cb1ad2bb
Joris Loemba <jorisloemba@gmail.com>
2018-06-03 21:26:42 +0100

    Hauteur refait

--- src/main.java
+++ src/main.java
@@ -1,22 +1,34 @@
-
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AB d = new AB("");
-		//AB d = new AB("230",e);
-		AB c = new AB("90");
-		AB b = new AB("150");
-		AB a = new AB("300",b,c);
+
+		AB g = new AB("927");
+		AB f = new AB("727");
+		AB e = new AB("431");		
+		AB d = new AB("100");		
+		AB c = new AB("800",f,g);
+		AB b = new AB("250",d,e);
+		AB a = new AB("688");
+
+		System.out.println("Hauteur de l'arbre");
+		System.out.println(a.hauteur());
+		System.out.println(" ");
 		
+		System.out.println("Taille de l'arbre");
+		System.out.println(a.taille());
+		System.out.println(" ");
 		
-		System.out.println(b.getDroite());
+		System.out.println("Parcours préfixe");
+		System.out.println(a.parcoursPrefixe());
+		System.out.println(" ");
 		
-		//System.out.println(a.hauteur());
-		//System.out.println(a.estEquilibre());
-		//System.out.println(a.taille());
-		//System.out.println(a.parcoursPrefixe());
+		System.out.println("est-il équilibré ? :");
+		System.out.println(a.estEquilibre());
+		System.out.println(" ");
+
+
 		
 	}
 
 }
+
