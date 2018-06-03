d815e65e0d6b5c63938a61f449a9d28a11987d72
Joris Loemba <jorisloemba@gmail.com>
2018-06-03 11:10:58 +0100

    Hauteur de l'arbre

--- src/AB.java
+++ src/AB.java
@@ -1,22 +1,24 @@
 public class AB {
 
-	private String racine;
-	private AB gauche;
-	private AB droite;
+	private String racine; // La racine du noeud
+	private AB gauche; //Le noeud externe de gauche
+	private AB droite;//Le noeud externe de droite
 	
 	
 	public AB(String racine, AB gauche, AB droite) {
-
 		this.racine = racine;
 		this.gauche = gauche;
 		this.droite = droite;
-		
 	}
 	
 	public AB(String racine,AB abGauche) {
 		this.racine = racine;
 		this.gauche = abGauche;
 		this.droite = null;
+	}
+	
+	public AB(){
+		
 	}
 	
 	public AB(String racine) {
@@ -32,58 +34,75 @@ public class AB {
 	public void setRacine(String racine) {
 		this.racine = racine;
 	}
+	
+	
+	//
 	public AB getGauche() {
-		return gauche;
+		if(this.gauche == null) return null;
+		return this.gauche;
 	}
 	public void setGauche(AB gauche) {
 		this.gauche = gauche;
 	}
+	
+	//
 	public AB getDroite() {
-		return droite;
+		if(this.droite == null) return null;
+		return this.droite;
 	}
 	public void setDroite(AB droite) {
 		this.droite = droite;
 	}
 		
 	
-	//Vérifie l'arbre est vide =>  A CORRIGER
+	//Vérifie l'arbre est vide =>  OK
 	public boolean estVide(){
-		return (this.equals(null));
+		if(this.getGauche() == null && this.getDroite() == null) return true;
+		else return false;
 	}
 	
-	
-	//Compte le nombre de noeud => Ok ! 
+	//Compte le nombre de noeud  ==> OK
 	public int taille(){
- 		 int result = 1;
- 	     if (!this.getGauche().estVide()) result += this.getGauche().taille();
- 	     if (!this.getDroite().estVide()) result += this.getDroite().taille();
+ 		 int result = 0;
+ 	     if (this.getGauche()!= null) result += 1+this.getGauche().taille();
+ 	     if (this.getDroite()!= null) result += 1+this.getDroite().taille();
  	     return result;
 	}
 	
 	
-	//Parcours préfixe => Ok ! 
+	//Parcours préfixe => en cours
 	public String parcoursPrefixe(){
-		 String parcours = this.getRacine();
- 	     if (!this.getGauche().estVide()) parcours += " "+this.getGauche().parcoursPrefixe()+" ";
- 	     if (!this.getDroite().estVide()) parcours += " "+this.getDroite().parcoursPrefixe()+" ";
- 	     return parcours;
+		 String parcours = this.getRacine()+" ";
+	     if (this.getGauche()!=null) parcours += this.getGauche().parcoursPrefixe();
+	     if (this.getDroite()!=null) parcours += this.getDroite().parcoursPrefixe();
+	     return parcours;
+	     
 	}
 	
-	
-	//Vérifie si la diff de hauteur des sous-arbres est entre 1 et -1. ==> OK
+	//Vérifie si la diff de hauteur des sous-arbres est entre 1 et -1. =>  A TESTER
 	public boolean estEquilibre(){
 		boolean result = false;
-		if((this.getDroite().hauteur()-this.getGauche().hauteur())<=1 || (this.getDroite().hauteur()-this.getGauche().hauteur())<-1) result = true;
+		if(this.getDroite().hauteur()>0 || this.getGauche().hauteur()>0){
+			 if(this.getDroite().hauteur()>0) return this.getDroite().estEquilibre();
+			 if(this.getGauche().hauteur()>0) return this.getGauche().estEquilibre();
+		}else{
+			int diff = this.getGauche().hauteur()-this.getDroite().hauteur();
+			result = (diff <2 && diff > -2);
+		}
 		return result;
 	}
 	
-	//Compter la hauteur de l'arbre binaire ==> OK
+	//Compter la hauteur de l'arbre binaire ==> Ne fonctionne pas
 	public int hauteur(){
-		int hauteur = 0;
-		if(this.getGauche() != null || this.getDroite() != null){
-			if(this.getGauche() != null) return 1+ this.getGauche().hauteur();
-			if(this.getDroite() != null) return 1+ this.getDroite().hauteur();
-		}else hauteur = 1; return hauteur;
+		int hauteur=0;
+		if (this.estVide()) hauteur += -1;
+        else{
+        	if(!this.getGauche().estVide() || !this.getGauche().estVide()){
+        		if(!this.getGauche().estVide() && this.getGauche()!=null) hauteur += 1+hauteur+this.getGauche().hauteur();
+        		if(!this.getDroite().estVide() && this.getDroite()!=null) hauteur += 1+hauteur+this.getDroite().hauteur();
+        	}
+        }
+		return hauteur;
 	}
 	
 	
